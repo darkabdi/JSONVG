f3364513c571940cc60b061fd33b4f027ef7d622
abdi mohamed <aabdymohamed@gmail.com>
2021-11-29 15:56:56 +0100

    upladdat

--- JSON.js
+++ JSON.js
@@ -1,17 +1,33 @@
 let input = document.getElementById("input")
-let url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=rum"
-let searchBtn = document.getElementById("search",(e)=>{
-e.preventDefault()
-fetchErryting()
+let card = document.getElementById("card")
+let cardContainer = document.getElementById("card-container")
+let url = "https://pokeapi.co/api/v2/pokemon/"
+let searchBtn = document.getElementById("search").addEventListener("click",(e)=>{
+    e.preventDefault()
+    fetchErryting()
 })
 
 
 
 
-async function fetchErryting(e){
-    e.preventDefault()
-    let response = await fetch(url+input.value)
-    let data = response.json()
-
+async function fetchErryting(){
+   try {
+    let response =  await fetch(url+input.value)
+    let data =      await response.json()
     console.log(data)
+    console.log(data.name)
+    console.log(data.abilities)
+    card.innerHTML += `<h2>${data.name}</h2>
+    <img src=\"${data.sprites.front_default}" alt=""\>
+    <p>abilities</p>
+
+    
+    `
+    
+    cardContainer +=`<li>${card}</lil>`
+   } catch (error) {
+       console.log("error!")
+       card.innerHTML += `<p>this pokemon doesn't exist bruhh</p>`
+
+   }
 }
