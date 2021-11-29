e68d1fab069b2e4f8cc3dcf740cc02c2f83d829b
abdi mohamed <aabdymohamed@gmail.com>
2021-11-29 10:59:19 +0100

    huh?

---
+++ JSON.js
@@ -0,0 +1,17 @@
+let input = document.getElementById("input")
+let url = "www.thecocktaildb.com/api/json/v1/1/search.php?s=rum"
+let searchBtn = document.getElementById("search",(e)=>{
+e.preventDefault()
+fetchErryting()
+})
+
+
+
+
+async function fetchErryting(e){
+    e.preventDefault()
+    let response = await fetch(url+input.value)
+    let data = response.json()
+
+    console.log(data)
+}
