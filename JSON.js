3bafef6f7b29ae025b175423ffa39a9a143d9341
abdi mohamed <aabdymohamed@gmail.com>
2021-11-29 18:35:50 +0100

    almosr done!!!

--- JSON.js
+++ JSON.js
@@ -1,33 +1,49 @@
 let input = document.getElementById("input")
+let optionName = document.getElementById("color-name")
 let card = document.getElementById("card")
 let cardContainer = document.getElementById("card-container")
 let url = "https://pokeapi.co/api/v2/pokemon/"
-let fetchTypeUrl = "https://pokeapi.co/api/v2/pokemon-color/{id or name}/"
 let searchBtn = document.getElementById("search").addEventListener("click",(e)=>{
     e.preventDefault()
-    fetchErryting()
+   if (optionName.value == "name"){
+       fetchName()
+   }
+   else fetchColour()
 })
 
 
 
 
-async function fetchErryting(){
+
+async function fetchName(){
+
    try {
     let response =  await fetch(url+input.value)
     let data =      await response.json()
     console.log(data)
-    
     card.innerHTML += `<h2>${data.name}</h2>
-    <img src=\"${data.sprites.front_default}" alt=""\>
-    <p id="abilities" >abilities</p>
-    `
-    let abilities=document.getElementById("abilities").addEventListener("click",()=>{
-       card.innerHTML += `<li>${data.abilities[0]}</li>`
-    })
+    <img src=\"${data.sprites.front_default}" alt=""\>`
     cardContainer +=`<li>${card}</lil>`
    } catch (error) {
        console.log("error!")
        card.innerHTML += `<p>this pokemon doesn't exist bruhh</p>`
 
    }
+}
+
+async function fetchColour(){
+    try {
+        let response =  await fetch(`https://pokeapi.co/api/v2/pokemon-color/${input.value}/`)
+        let data =      await response.json()
+        console.log(data.pokemon_species)
+        let cardHTml = ""
+        for (let poke of data.pokemon_species ){
+            cardHTml += `<h2>${poke.name}</h2>`
+            cardContainer.innerHTML +=cardHTml
+        }
+       console.log(poke.name)
+        
+       } catch (error) {
+           console.log("error!")
+        }
 }
